5b3830edd6ecb34f6e6c4c64b9b0ce8204daf7fe
IMYONGJIN\dksp5 <dksp56@naver.com>
2024-10-02 05:18:25 +0900

    복습

---
+++ src/main/java/com/quiz/lesson03/Lesson03Quiz01RestController.java
@@ -0,0 +1,41 @@
+package com.quiz.lesson03;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.quiz.lesson03.bo.RealEstateBO;
+import com.quiz.lesson03.domain.RealEstate;
+
+@RestController // Controller + ResponseBody
+@RequestMapping("/lesson03/quiz01")	 // 공통 경로 
+public class Lesson03Quiz01RestController {
+
+	@Autowired // 의존성 주입 
+	private RealEstateBO realEstateBO;
+	//private RealEstateMapper mapper; 가능은 하지만 코드의 유지보수 때문에 비추
+
+	// http://localhost:8080/lesson03/quiz01/1?id=20
+	@RequestMapping("/1")	
+	public RealEstate quiz01_1(
+			@RequestParam("id") int id){
+		return realEstateBO.getRealEstateById(id);
+	}
+	// http://localhost:8080/lesson03/quiz01/2?rent_price=90
+	@RequestMapping("/2")	
+	public List<RealEstate> quiz01_2 (
+		@RequestParam(value = "rent_price") int rentPrice		
+			) {
+		return realEstateBO.getRealEstateListByRentPrice(rentPrice); 
+	}
+	// http://localhost:8080/lesson03/quiz01/3?area=90&price=130000
+		@RequestMapping("/3")	
+		public List<RealEstate> quiz01_3(
+			@RequestParam("area") int area,
+			@RequestParam("price") int price){
+		return	realEstateBO.getRealEstateListByAreaPrice(area, price);
+	}
+}
---
+++ src/main/java/com/quiz/lesson03/mapper/RealEstateMapper.java
@@ -0,0 +1,30 @@
+package com.quiz.lesson03.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import com.quiz.lesson03.domain.RealEstate;
+
+@Mapper
+public interface RealEstateMapper {
+	
+	// input : id(int)
+	// output : RealEstate(DTO)
+	public RealEstate selectRealEstateById(int id);
+
+	// input : rentPrice(Integer)
+	// output : List<RealEstate>
+	public List<RealEstate> selectRealEstateListByRentPrice(Integer  rentPrice);
+
+	// input :  area(int), price(int)
+	// output : List<RealEstate>
+	public List<RealEstate> selectRealEstateListByAreaPrice(
+			// 파라미터를 두개 이상 xml에 보낼 수 없다. 
+			// 하나의 맵으로 구성해야 하는데, @Param이 맵으로 만들어준다.
+			// Param("") <= 는 xml의 #{}와 연결
+			@Param("area") int area, 
+			@Param("price") int price);
+
+}
