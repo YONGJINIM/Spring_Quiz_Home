c0f0706600c6276b391f170da76e03fdff6c4187
IMYONGJIN\dksp5 <dksp56@naver.com>
2024-10-21 04:19:53 +0900

    10-21

--- src/main/java/com/quiz/lesson06/Lesson06Quzi01Controller.java
+++ src/main/java/com/quiz/lesson06/Lesson06Quzi01Controller.java
@@ -1,16 +1,28 @@
 package com.quiz.lesson06;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.quiz.lesson06.bo.BookmarkBO;
+import com.quiz.lesson06.domain.Bookmark;
+
 @Controller
 @RequestMapping("/lesson06/quiz01")
 public class Lesson06Quzi01Controller {
 
+	@Autowired
+	private BookmarkBO bookmarkBO;
+	
 	// 즐겨찾기 추가하기 화면 
 	// http://localhost:8080/lesson06/quiz01/add-bookmark-view
 	@GetMapping("/add-bookmark-view")
@@ -19,18 +31,37 @@ public class Lesson06Quzi01Controller {
 	}
 	
 	// 추가 기능 => AJAX 호출
+	// @ResponseBody가 사용되면 Model을 사용할 수 없다.(기억!)
 	@ResponseBody
 	@PostMapping("/add-bookmark")
-	public String addBookmark(
+	public Map<String, Object> addBookmark(
 			@RequestParam("name") String name ,
 			@RequestParam("url") String url
 			) {
-		return "성공"; 
-	}
 		
+		// DB Insert
+		bookmarkBO.addBookmark(name, url);
+		
+		// 성공 여부 JSON String
+		// "{"code":200, "result":"성공"}"
+		Map<String, Object> result = new HashMap<>();
+		result.put("code", 200);
+		result.put("result", "성공");
+		return result; 
+	}
 	
+	// 즐겨찾기 목록화면
+	@GetMapping("/bookmark-list-view")
 	
-	
-	
+	public String bookmarkListView(Model model) {
+		// DB select => List
+		List<Bookmark> bookmarkList = bookmarkBO.selectBookmarkList();
+		
+		// model에 담기
+		model.addAttribute("bookmarkList",bookmarkList);
+		
+		// view 화면 이동
+		return"lesson06/bookmarkList";
+	}
 	
 }
---
+++ src/main/java/com/quiz/lesson06/mapper/BookmarkMapper.java
@@ -0,0 +1,26 @@
+package com.quiz.lesson06.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import com.quiz.lesson06.domain.Bookmark;
+
+@Mapper
+public interface BookmarkMapper {
+
+	
+	//input : name, url 
+	//output : void 또는 int 
+	
+	public void insertBookmark(
+			@Param("name") String name,
+			@Param("url") String url 
+			);
+	
+	// input : X
+	// output : List<Bookmark>
+	
+	public List<Bookmark> selectBookmarkList();
+}
