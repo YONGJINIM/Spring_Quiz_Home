46ebc990d2334db141fea356ab6328b856537cf2
IMYONGJIN\dksp5 <dksp56@naver.com>
2024-10-28 19:24:20 +0900

    10-28

--- src/main/java/com/quiz/lesson07/Lesson07Quiz02RestController.java
+++ src/main/java/com/quiz/lesson07/Lesson07Quiz02RestController.java
@@ -1,5 +1,6 @@
 package com.quiz.lesson07;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,15 +21,57 @@ public class Lesson07Quiz02RestController {
 	
 	// http://localhost:8080/lesson07/quiz02/1
 	@GetMapping("/1")
-	public RecruitEntity idSelect_1() { // 단건
+	public RecruitEntity quiz01() { // 단건
 		return recruitRepository.findById(8);
 	}
 	// http://localhost:8080/lesson07/quiz02/2?companyId=2
 	@GetMapping("/2")
-	public List<RecruitEntity> idSelect_2(
+	public List<RecruitEntity> quiz02(
 			@RequestParam("companyId") int companyId
 			) {
 		return recruitRepository.findByCompanyId(companyId);
 	}
+	// http://localhost:8080/lesson07/quiz02/3
+	@GetMapping("/3")
+	public List<RecruitEntity> quiz03() {
+		return recruitRepository.findByPositionContainsAndType("back-end","정규직");
+	}
+	// http://localhost:8080/lesson07/quiz02/4
+	@GetMapping("/4")
+	public List<RecruitEntity> quiz04() {
+		String type = "정규직";
+		int salary = 9000;
+		// 넘길 인자의 순서가 중요하다. 
+		return recruitRepository.findByTypeOrSalaryGreaterThanEqual(type , salary);
+	}
 	
+	/*
+	 http://localhost:8080/lesson07/quiz02/5
+	 
+	 Top3와 같은 키워드는 정렬 조건 이후가 아니라 앞쪽에 와야 합니다. 
+	 JPA 메소드 네이밍 규칙에서 예약어의 순서가 정해져 있기 때문입니다. 
+	 findTop3By는 반환할 최대 개수를 지정하는 예약어로, 
+	 이와 같은 키워드는 메소드 이름의 조건과 정렬 기준보다 앞에 나와야 합니다.
+	 
+	 */
+	@GetMapping("/5")
+	public List<RecruitEntity> quiz05(){
+		 return recruitRepository.findTop3ByTypeOrderBySalaryDesc("계약직");
+	}
+	// http://localhost:8080/lesson07/quiz02/6
+	@GetMapping("/6")
+	public List<RecruitEntity> quiz06() {
+		return recruitRepository.findByRegionAndSalaryBetween("성남시 분당구" ,7000,8500);
+	}
+	// http://localhost:8080/lesson07/quiz02/7
+	@GetMapping("/7")
+	public List<RecruitEntity> quiz07(){
+		
+		int salary = 8100;
+		String type ="정규직";
+		LocalDate deadline = LocalDate.of(2026, 4, 10); // 2026-04-10 이후 조건
+		
+		// findBySalaryGreaterThanAndTypeAndDeadlineAfterOrderBySalaryDesc
+		return recruitRepository.findCondition(salary, type, deadline);
+	}
 }
--- src/main/java/com/quiz/lesson07/repository/RecruitRepository.java
+++ src/main/java/com/quiz/lesson07/repository/RecruitRepository.java
@@ -1,8 +1,11 @@
 package com.quiz.lesson07.repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.quiz.lesson07.entity.RecruitEntity;
 
@@ -13,4 +16,24 @@ public interface RecruitRepository extends JpaRepository<RecruitEntity, Integer>
 	
 	// JPQL => Entity에 조회하는 것
 	public List<RecruitEntity> findByCompanyId(int companyId);
+	
+	public List<RecruitEntity> findByPositionContainsAndType(String Position , String Type);
+
+	public List<RecruitEntity> findByTypeOrSalaryGreaterThanEqual(String type, int salary);
+
+	public List<RecruitEntity> findTop3ByTypeOrderBySalaryDesc(String type);
+	
+	public List<RecruitEntity> findByRegionAndSalaryBetween(String region, int minSalary, int maxSalary);
+	 
+	// Native Query를 사용한 조회
+    @Query(value = "SELECT * FROM recruit " +
+                   "WHERE salary >= :salary " +
+                   "AND type = :type " +
+                   "AND deadline > :deadline " +
+                   "ORDER BY salary DESC", nativeQuery = true)
+    
+    public List<RecruitEntity> findCondition(
+        @Param("salary") int salary, 
+        @Param("type") String type, 
+        @Param("deadline") LocalDate deadline);
 }
